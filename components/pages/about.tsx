25497df739410631b62ad57f0c1b3755c842db72
Nishant Mishra <nishdrummer@gmail.com>
2024-08-13 05:31:37 +0530

    feat: Added about section

--- components/pages/about.tsx
+++ components/pages/about.tsx
@@ -7,13 +7,13 @@ export default function About() {
       <p className="text-xs ">
         ABOUT US
         </p>
-        <h1 className="marcellus text-4xl font-light">Redefining Hospitality with Innovation and Integration</h1>
+        <h1 className="marcellus text-2xl sm:text-4xl font-light">Redefining Hospitality with Innovation and Integration</h1>
       </div>
-      <div className="flex flex-col gap-3 md:w-1/2 p-3 pt-10">
-        <p className="text-sm">
+      <div className="flex flex-col gap-3 md:w-1/2 p-3 sm:pt-10">
+        <p className="text-xs leading-6 sm:text-sm">
           Manago is a team of F&B experts who act as personal managers, helping you set up restaurants, cafes, or hotels. We provide comprehensive consulting services, from concept to launch, including in-house services. Our all-in-one approach saves time and capital, ensuring a well-planned and budget-friendly setup.
         </p>
-        <p className="text-sm">
+        <p className="text-xs leading-6 sm:text-sm">
           At MANAGO, our vision is to redefine the hospitality landscape in Bangalore by seamlessly integrating diverse services to empower businesses. We envision becoming the go-to-partner for establishments in the hospitality industry, providing not only skilled manpower but also the essential infrastructure and resources necessary for success. Our aim is to enhance operational efficiency and elevate the overall guest experience, establishing MANAGO as the cornerstone of excellence in the hospitality sector.
         </p>
       </div>
